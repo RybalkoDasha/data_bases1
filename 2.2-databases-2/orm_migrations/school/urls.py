48d072fecd62bffb49d6dddc38a1d3361ee79977
RybalkoDasha <90975577+RybalkoDasha@users.noreply.github.com>
2023-09-25 17:25:52 +0300

    Delete 2.2-databases-2 directory

--- 2.2-databases-2/orm_migrations/school/urls.py
+++
@@ -1,9 +0,0 @@
-import debug_toolbar
-from django.urls import include, path
-
-from school.views import students_list
-
-urlpatterns = [
-    path('', students_list, name='students'),
-    path('__debug__/', include(debug_toolbar.urls)),
-]
